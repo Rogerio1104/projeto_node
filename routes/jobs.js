a31e475b390ddab2f7437efd9c1ade08b3df9967
Rogerio Ricardo de Avila <rogerio.avila@unimedcriciuma.com.br>
2023-12-12 15:29:38 -0300

    Criando as views e ajustando as rotas

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,28 +1,42 @@
 const express = require('express');
 const router  = express.Router();
-const job     = require('../models/Job');
+const Job     = require('../models/Job');
 
-router.get('/test',(req,res)=>{
-    res.send('deu certo');
+
+// detalhe da vaga  view/1, view/2
+router.get('/view/:id', (req, res) => Job.findOne({
+  where: {id: req.params.id}
+}).then(job => {
+
+  res.render('view', {
+    job
   });
-  
+
+}).catch(err => console.log(err)));
+
+
+// form da rota de envio
+router.get('/add', (req, res) => {
+  res.render('add');
+})
 
 // add job via post
-router.post('/add',(req,res) =>{
-    
-    let {title,salary,company, description, email, new_job} = req.body;
+router.post('/add', (req, res) => {
 
-    //insert 
-    job.create({
-        title,
-        description,
-        salary,
-        email,
-        company,
-        new_job
-    })
-    .then(() => res.redirect('/'))
-    .catch(err => console.log(err));
+  let {title, salary, company, description, email, new_job} = req.body;
+
+  // insert de dados no banco
+  Job.create({
+    title,
+    description,
+    salary,
+    company,
+    email,
+    new_job
+  })
+  .then(() => res.redirect('/'))
+  .catch(err => console.log(err));
+
 });
 
 module.exports = router
